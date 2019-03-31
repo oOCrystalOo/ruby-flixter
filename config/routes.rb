ffc9a25a2aa69173e6479213244682323409f5d8
Crystal Ng <ng.crystal.wy@gmail.com>
2019-03-31 01:37:19 +0000

    Added lessons action and authentication before adding lesson

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
   root 'static_pages#index'
   
   namespace :instructor do
-    resources :courses, only: [:new, :create, :show, :destroy] do
-      resources :sections, only: [:new, :create, :destroy]
+    resources :sections, only: [] do
+      resources :lessons, only: [:new, :create]
+    end
+    resources :courses, only: [:new, :create, :show] do
+      resources :sections, only: [:new, :create]
     end
   end
   
