3e5148b03224b147ea69282d9c58cbb3da3771a0
Crystal Ng <ng.crystal.wy@gmail.com>
2019-04-15 03:46:53 +0000

    Added finishing touches

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -9,10 +9,16 @@ class LessonsController < ApplicationController
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+  
+  # Check if user is enrolled in course, or if is instructor
   def check_enrollment 
-    unless current_user && current_user.enrolled_in?(current_lesson.section.course)
+    unless current_user && (current_user.enrolled_in?(current_lesson.section.course) || current_user === current_lesson.section.course.user)
       flash.alert = "You are not enrolled in this course"
       redirect_to course_path(current_lesson.section.course)
+    end
+    
+    if current_user === current_lesson.section.course.user
+      flash.notice = "You are viewing your lesson as a student"
     end
   end
 end
